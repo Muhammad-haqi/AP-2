d62c18173b7628d8895d4da3d809783a1ab5a5eb
Muhammad-haqi <haqyrasya21@gmail.com>
2025-05-25 15:53:45 +0700

    pertemuan 2 : penggunaan operator

---
+++ PERTEMUAN 2/luasPpanjang.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+system("CLS");
+
+    float luas, p, l;
+
+    cout << "masukan panjang :" ;
+    cin >> p;
+
+    cout << "masukan lebar :" ;
+    cin >> l;
+
+    luas = p * l;
+    cout << "luas = " << luas << endl;
+
+    return 0 ;
+}
