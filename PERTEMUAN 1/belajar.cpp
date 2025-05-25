fd585f4aeeb593b8dd5c299ae795629175fe7851
Muhammad-haqi <haqyrasya21@gmail.com>
2025-05-25 15:51:48 +0700

    pertemuan 1 : pengenalan operasi input output

--- PERTEMUAN 1/belajar.cpp
+++ PERTEMUAN 1/belajar.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
      string nama;
-     char kom , jenis kelamin ;
+     char kom , jenisKelamin ;
      float ip;
      int nim; 
 
@@ -15,21 +15,26 @@ int main() {
 
     cout << "masukan nama anda : ";
     cin >> nama ;
+
+    getline(cin,nama); // agar karakter spasi bisa terbaca
+
     cout << "masukan kom : ";
     cin >> kom ;
     cout << "masukan ip : ";
     cin >> ip ;
-    cout << " masukan nim : ";
+    cout << "masukan nim : ";
     cin >> nim ;
-    cout <<  "masukan jenis kelamin (L/P) : ";
-    jenis kelamin = getche (); //
-    getline(cin,nama); // agar karakter spasi bisa terbaca
+    cout << "masukan jenisKelamin (L/P) : ";
+    jenisKelamin = getche (); // agar karakter lngsung tampil tanpa menekan enter
+    
 
     /*untuk output*/
-    cout << "nama anda adalah: " << nama << endl;
+    cout << "\n" "nama anda adalah: " << nama << endl;
     cout << "kom anda adalah: " << kom << endl;
     cout << "ip anda adalah: " << ip << endl;
     cout << "nim anda adalah: " << nim << endl;
-    putchar (jenis kelamin); // untuk menampilkan karakter
+    putchar (jenisKelamin); // untuk menampilkan karakter
+
+    getch(); // karakter yang diketik ga di tampilkan di layar
 
 }
